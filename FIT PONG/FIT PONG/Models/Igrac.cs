f07da6dfd8e8022981680e0400732bb918c917a5
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2019-12-11 10:33:28 +0100

    U klasi igrac promijenjeno nasljedjivanje zbog entityframeworkovog razumijevanja i interpretiranja tabela,napravljena nova migracija sa ispravnim podacima

--- FIT PONG/FIT PONG/Models/Igrac.cs
+++ FIT PONG/FIT PONG/Models/Igrac.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FIT_PONG.Models
 {
-    public class Igrac : User
+    public class Igrac
     {
+        [Key()]
+        [ForeignKey(nameof(User))]
+        public int ID { get; set; }
+        public User User { get; set; }
+
         [StringLength(50)]
         public string PrikaznoIme { get; set; }
 
@@ -29,14 +35,7 @@ namespace FIT_PONG.Models
 
         }
 
-        public Igrac(string ime, string prezime, DateTime datumRodjenja, string email, int gradID, int loginID, string prikaznoIme="NOT SET", string jacaRuka="NOT SET", double visina=-1)
-            :base(ime,prezime,datumRodjenja,email,gradID,loginID)
-        {
-            PrikaznoIme = prikaznoIme;
-            JacaRuka = jacaRuka;
-            Visina = visina;
-            BrojPosjetaNaProfil = 0;
-        }
+
 
     }
 }
