55bdd8fdef98d4066bbf567b44855f689c3f6702
unknown <aldin.talic@edu.fit.ba>
2020-03-15 20:38:59 +0100

    Ispravljanje bugova u controlleru FitApi

--- FIT PONG/FIT PONG/Controllers/FitApiController.cs
+++ FIT PONG/FIT PONG/Controllers/FitApiController.cs
@@ -46,23 +46,38 @@ namespace FIT_PONG.Controllers
             List<int> l = new List<int>();
             if (!takm.Inicirano)
                 //vrati nesto ovdje puca program kad nije inicirano a udjes u rezultate
-                return "";
+                return null;
             var userName = httpContextAccessor.HttpContext.User.Identity.Name;
             Igrac igr = db.Igraci.Include(d => d.User).Where(d => d.User.Email == userName).SingleOrDefault();
 
-            for (int i = 0; i < takm.Bracketi[0].Runde.Count(); i++)
+            bool isTim = false;
+            int step = 2;
+            int x = 0;
+            if (takm.VrstaID == 2)
             {
-                for (int j = 0; j < takm.Bracketi[0].Runde[i].Utakmice.Count(); j++)
-                {
-                    if (takm.Bracketi[0].Runde[i].Utakmice[j].UcescaNaUtakmici[0].IgracID == igr.ID
-                        || takm.Bracketi[0].Runde[i].Utakmice[j].UcescaNaUtakmici[1].IgracID == igr.ID)
-                        l.Add(takm.Bracketi[0].Runde[i].Utakmice[j].BrojUtakmice);
-                }
+                isTim = true;
+                step = 4;
+                x = 2;
             }
 
-            var lista = (from t in db.Takmicenja
-                         join b in db.Brackets
-                             on t.ID equals b.TakmicenjeID
+            var IgraciUtakmiceIDevi = (from iu in db.IgraciUtakmice
+                                       join u in db.Utakmice
+                                            on iu.UtakmicaID equals u.ID
+                                       join r in db.Runde
+                                           on u.RundaID equals r.ID
+                                       join b in db.Brackets
+                                           on r.BracketID equals b.ID
+                                       where b.TakmicenjeID == id
+                                       select new
+                                       {
+                                           iu.IgID
+                                       }).OrderBy(d=>d.IgID).ToList();
+
+            int minID = IgraciUtakmiceIDevi.First().IgID;
+            int maxID = IgraciUtakmiceIDevi.Last().IgID;
+
+
+            var lista = (from b in db.Brackets
                          join r in db.Runde
                              on b.ID equals r.BracketID
                          join u in db.Utakmice
@@ -73,20 +88,24 @@ namespace FIT_PONG.Controllers
                          from pi in tempJoin.DefaultIfEmpty()
                          join p in db.Prijave on pi.PrijavaID equals p.ID into tJoin
                          from p in tJoin.DefaultIfEmpty()
-                         where t.ID == id 
+                         where p.TakmicenjeID == id && iu.IgID>=minID && iu.IgID<=maxID
                          select new 
                          {
                              IgUtId = iu.IgID,
                              Naziv = p.Naziv,
                              OsvojeniSetovi = iu.OsvojeniSetovi,
                              BrojUtakmice = u.BrojUtakmice
-                         }).OrderBy(x => x.BrojUtakmice).ToList();
+                         }).OrderBy(x => x.BrojUtakmice).ThenBy(x=>x.Naziv).ToList();
             lista = lista.DistinctBy(x => new { x.IgUtId }).ToList();
 
             List<(string tim1, int? set1, int? set2, string tim2,int IgId1, int IgId2) > parovi = new List<(string tim1, int? set1, int? set2, string tim2, int IgId1,int IgId2)>();
-            for (int i = 0; i < lista.Count(); i += 2)
+            for (int i = 0; i <lista.Count()-x; i+=step)
             {
-                parovi.Add((lista[i].Naziv, lista[i].OsvojeniSetovi, lista[i + 1].OsvojeniSetovi, lista[i + 1].Naziv, lista[i].IgUtId,lista[i+1].IgUtId));
+                if (!isTim)
+                    parovi.Add((lista[i].Naziv, lista[i].OsvojeniSetovi, lista[i + 1].OsvojeniSetovi, lista[i + 1].Naziv, lista[i].IgUtId, lista[i + 1].IgUtId));
+                else
+                    parovi.Add((lista[i].Naziv, lista[i].OsvojeniSetovi, lista[i + 2].OsvojeniSetovi, lista[i + 2].Naziv, lista[i].IgUtId, lista[i + 2].IgUtId));
+
             }
 
             var jsonObj = JsonConvert.SerializeObject(parovi);
