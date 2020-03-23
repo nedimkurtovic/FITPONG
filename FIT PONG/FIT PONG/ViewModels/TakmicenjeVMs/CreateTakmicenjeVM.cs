70383a59478432f8fb5138d91d951e06df7c8157
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-03-23 16:44:35 +0100

    poliranje frontenda v4
    Promjene na takmicenje dodaj viewu

--- FIT PONG/FIT PONG/ViewModels/TakmicenjeVMs/CreateTakmicenjeVM.cs
+++ FIT PONG/FIT PONG/ViewModels/TakmicenjeVMs/CreateTakmicenjeVM.cs
@@ -21,16 +21,30 @@ namespace FIT_PONG.ViewModels.TakmicenjeVMs
         public DateTime? RokZavrsetkaPrijave { get; set; }
 
         [RegularExpression(@"^[0-9]+$")]
+        [Display(Name = "Minimalni ELO")]
         public int? MinimalniELO { get; set; }
+        [Display(Name = "Rasporedi igrace na osnovu elo? (prazno oznacava random)")]
         public bool Seeded { get; set; }
+        
+        [Display(Name ="Kategorija")]
         public int KategorijaID { get; set; }
+        
+        [Display(Name = "Sistem")]
         public int SistemID { get; set; }
+
+        [Display(Name ="Vrsta")]
         public int VrstaID { get; set; }
+
+        [Display(Name = "Status")]
         public int StatusID { get; set; }
+        [Display(Name = "Rucni unos prijava?(Trenutno samo za singleove dostupno, u suprotnom definisite registracije)")]
         public bool RucniOdabir { get; set; }
+        [Display(Name = "Korisnicka imena igraca (format : @KorisnickoIme razmak)")]
         public string RucnoOdabraniIgraci { get; set; }
+        [Display(Name = "Datum pocetka")]
         public DateTime? DatumPocetka { get; set; }
         //ne smije biti manji od datuma pocetka
+        [Display(Name = "Datum zavrsetka")]
         public DateTime? DatumZavrsetka { get; set; }
     }
 }
