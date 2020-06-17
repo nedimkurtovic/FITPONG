29ccf2a7d8bbd1de7ac0ffda911c6b3f545d35ca
talicni_tom <aldin.talic@edu.fit.ba>
2020-06-17 21:40:10 +0200

    Get metoda u user servisima

--- FIT PONG/FITPONG.Services/Services/UsersService.cs
+++ FIT PONG/FITPONG.Services/Services/UsersService.cs
@@ -1,4 +1,5 @@
-﻿using FIT_PONG.Database;
+﻿using AutoMapper;
+using FIT_PONG.Database;
 using FIT_PONG.Services.BL;
 using FIT_PONG.Services.Services;
 using FIT_PONG.SharedModels;
@@ -6,6 +7,7 @@ using FIT_PONG.SharedModels.Requests.Account;
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace FIT_PONG.Services.Services
@@ -16,13 +18,15 @@ namespace FIT_PONG.Services.Services
         private readonly UserManager<IdentityUser<int>> usermanager;
         private readonly SignInManager<IdentityUser<int>> signinmanager;
         private readonly iEmailServis mailservis;
+        private readonly Mapper mapper;
         public UsersService(MyDb _db, SignInManager<IdentityUser<int>> _singinmanager,
-            UserManager<IdentityUser<int>> _usermanager, iEmailServis _mailservis)
+            UserManager<IdentityUser<int>> _usermanager, iEmailServis _mailservis, Mapper mapper)
         {
             db = _db;
             usermanager = _usermanager;
             signinmanager = _singinmanager;
             mailservis = _mailservis;
+            this.mapper = mapper;
         }
 
         public Users ConfirmEmail(int id, string token)
@@ -37,7 +41,13 @@ namespace FIT_PONG.Services.Services
 
         public List<Users> Get(AccountSearchRequest obj)
         {
-            throw new NotImplementedException();
+            var users=new List<Database.DTOs.Igrac>();
+            if (obj.PrikaznoIme == null)
+                users = db.Igraci.ToList();
+            else
+                users = db.Igraci.Where(x => x.PrikaznoIme.Contains(obj.PrikaznoIme)).ToList();
+
+            return mapper.Map<List<SharedModels.Users>>(users);
         }
 
         public Users Get(int ID)
