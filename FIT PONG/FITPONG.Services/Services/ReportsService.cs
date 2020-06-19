f8e3adabfc551d2318f68023378e5ae6aad72fe4
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-06-19 23:07:17 +0200

    Implementiran reports service
    Nije testirano, kod je prebacen iz web appa uz manje modifikacije, potrebno otkriti kako dijeliti folder tj static fajlove izmedju webapp i webapija

--- FIT PONG/FITPONG.Services/Services/ReportsService.cs
+++ FIT PONG/FITPONG.Services/Services/ReportsService.cs
@@ -13,87 +13,92 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime;
 using System.Text;
+using FIT_PONG.Services.BL;
 
 namespace FIT_PONG.Services.Services
 {
     public class ReportsService : BaseService<SharedModels.Reports, Database.DTOs.Report, object>, IReportsService
     {
-        public ReportsService(MyDb _context, IMapper _mapko):base(_context,_mapko)
+        private readonly iEmailServis emailServis;
+
+        public ReportsService(MyDb _context, IMapper _mapko, iEmailServis _emailServis):base(_context,_mapko)
         {
-            
+            emailServis = _emailServis;
         }
 
-        public Reports Add(ReportsInsert obj)
+        public bool Validiraj(ReportsInsert obj)
         {
-            throw new NotImplementedException();
+            UserException ex = new UserException();
+            if (!OdgovarajuciFajlovi(obj.Prilozi))
+                ex.AddError(nameof(obj.Prilozi), "Možete uploadovati samo slike");
+            if (ex.Errori.Count > 0)
+                throw ex;
+            return true;
         }
-        //public bool Validiraj(ReportsInsert obj)
-        //{
-        //    UserException ex = new UserException();
-        //    if (!OdgovarajuciFajlovi(obj.Prilozi))
-        //        ex.AddError(nameof(obj.Prilozi), "Možete uploadovati samo slike");
-        //    if (ex.Errori.Count > 0)
-        //        throw ex;
-        //    return true;
-        //}
 
-        //private bool OdgovarajuciFajlovi(List<byte[]> prilozi)
-        //{
-        //    if (prilozi == null || prilozi.Count == 0)
-        //        return true;
-        //    foreach (byte[] i in prilozi) 
-        //    {
-        //        //logika
-        //        //ukratko izgleda da se ne moze saznati tip fajla ako je byte stream..
-        //        //samo da je skontat kako uvalit iformfile umjesto byte streama 
-        //    }
-        //}
+        private bool OdgovarajuciFajlovi(List<Fajl> prilozi)
+        {
+            if (prilozi == null || prilozi.Count == 0)
+                return true;
+            foreach (Fajl i in prilozi)
+            {
+                int poz = i.Naziv.LastIndexOf(".");
+                string ekstenzija = i.Naziv.Substring(poz);
+                if (ekstenzija != "png" || ekstenzija != "jpg")
+                    return false;
+            }
+            return true;
+        }
 
-        //public SharedModels.Reports Add(ReportsInsert obj) // razmisliti da se iz httpcontext. izvuce root path aplikacije kako bi se
-        //    //uploadovo fajl tj da se ovoj metodi posalje taj parametar a ne ovako da ovaj nagadja sa ~/reports
-        //{
-        //    Validiraj(obj);
-        //    using (var transakcija = db.Database.BeginTransaction())
-        //    {
-        //        List<string> bekapPlan = new List<string>();
-        //        try
-        //        {
-        //            var bazaObj = mapko.Map<Database.DTOs.Report>(obj);
-        //            bazaObj.Prilozi = new List<Database.DTOs.Attachment>();
-        //            db.Reports.Add(bazaObj);
-        //            db.SaveChanges();
-        //            if(obj.Prilozi != null)
-        //            {
-        //                foreach(byte[] i in obj.Prilozi)
-        //                {
-        //                    using(MemoryStream ms = new MemoryStream(i))
-        //                    {
-        //                        Directory.CreateDirectory("~/reports");
-        //                        string ImeFajla = Guid.NewGuid().ToString();
-        //                        string pathSpremanja = Path.Combine("~/reports", ImeFajla);
-        //                        using (FileStream strim = new FileStream(pathSpremanja,FileMode.Create))
-        //                            ms.CopyTo(strim);
-        //                        bekapPlan.Add(pathSpremanja);
-        //                        Attachment noviAttachment = new Attachment { DatumUnosa = DateTime.Now, Path = pathSpremanja };
-        //                        db.Attachments.Add(noviAttachment);
-        //                    }
-        //                }
-        //                db.SaveChanges();
-        //                transakcija.Commit();
-        //                //ovdje treba pozvati email servis i obavijestiti adminsitratora o novom reportu
-        //            }
-        //        }
-        //        catch (DbUpdateException)
-        //        {
-        //            transakcija.Rollback();
-        //            foreach (string i in bekapPlan)
-        //                if (System.IO.File.Exists(i))
-        //                    System.IO.File.Delete(i);
-        //        }
+        public SharedModels.Reports Add(ReportsInsert obj , string rootPathAplikacije = "~") // razmisliti da se iz httpcontext. izvuce root path aplikacije kako bi se
+         //uploadovo fajl tj da se ovoj metodi posalje taj parametar a ne ovako da ovaj nagadja sa ~/reports
+        {
+            Validiraj(obj);
+            using (var transakcija = db.Database.BeginTransaction())
+            {
+                List<string> bekapPlan = new List<string>();
+                try
+                {
+                    var bazaObj = mapko.Map<Database.DTOs.Report>(obj);
+                    bazaObj.Prilozi = new List<Database.DTOs.Attachment>();
+                    db.Reports.Add(bazaObj);
+                    db.SaveChanges();
+                    if (obj.Prilozi != null)
+                    {
+                        foreach (Fajl i in obj.Prilozi)
+                        {
+                            using (MemoryStream ms = new MemoryStream(i.BinarniZapis))
+                            {
+                                //valja istraziti tj kako spremiti u zajednicki folder za webapp i webapi
+                                Directory.CreateDirectory(Path.Combine(rootPathAplikacije,"reports"));
+                                string ImeFajla = Guid.NewGuid().ToString() + "_" + i.Naziv;
+                                string pathSpremanja = Path.Combine(rootPathAplikacije, "reports", ImeFajla);
+                                using (FileStream strim = new FileStream(pathSpremanja, FileMode.Create))
+                                    ms.CopyTo(strim);
+                                bekapPlan.Add(pathSpremanja);
+                                Attachment noviAttachment = new Attachment { DatumUnosa = DateTime.Now, Path = pathSpremanja };
+                                db.Attachments.Add(noviAttachment);
+                            }
+                        }
+                        db.SaveChanges();
+                        transakcija.Commit();
+                        //ovdje treba pozvati email servis i obavijestiti adminsitratora o novom reportu
+                        emailServis.PosaljiMejlReport(bazaObj);
+                        return mapko.Map<SharedModels.Reports>(bazaObj);
+                    }
+                }
+                catch (DbUpdateException)
+                {
+                    transakcija.Rollback();
+                    foreach (string i in bekapPlan)
+                        if (System.IO.File.Exists(i))
+                            System.IO.File.Delete(i);
+                }
+                UserException ex = new UserException();
+                ex.AddError("error", "Došlo je do greške prilikom pohrane fajlova, pokušajte ponovo");
+                throw ex;
+            }
 
-
-        //    }
-
-        //}
+        }
     }
 }
