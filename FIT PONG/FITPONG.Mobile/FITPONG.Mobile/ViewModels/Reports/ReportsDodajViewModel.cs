abe5edd9d26af8b08bfb9eba129793608af61776
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-07-27 20:07:39 +0200

    2

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Reports/ReportsDodajViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIT_PONG.Mobile.ViewModels.Reports
+{
+    public class ReportsDodajViewModel: BaseViewModel
+    {
+    }
+}
