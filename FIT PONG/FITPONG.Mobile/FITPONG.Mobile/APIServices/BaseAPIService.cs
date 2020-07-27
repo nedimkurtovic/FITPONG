1de00eed355cbc0d4b5a555fb1f718a753ba3ad6
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-07-27 20:15:18 +0200

    Kreirane api servisi klase za komunikaciju sa takmicenje i user kontrolerima i base servis za opcenitu upotrebu
    Users i takmicenje api servisi su bazirani na base servisu koji sadrzi username i password atribute kako bi se koristili pri pozivima apiju

--- FIT PONG/FITPONG.Mobile/FITPONG.Mobile/APIServices/BaseAPIService.cs
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/APIServices/BaseAPIService.cs
@@ -12,11 +12,14 @@ namespace FIT_PONG.Mobile.APIServices
     {
         public string resurs { get; set; }
         //public string APIUrl = $"{Resources.ApiUrl}";
-        public static string Username { get; set; }
-        public static string Password { get; set; }
+        //public static string Username { get; set; }
+        //public static string Password { get; set; }
+        public static string Username { get; set; } = "nedim.kurtovic@edu.fit.ba";
+        public static string Password { get; set; } = "nedim123.";
+
 
 #if DEBUG
-        protected string APIUrl = "http://localhost:44391/api";
+        protected string APIUrl = "http://localhost:5766/api";
 #endif
 #if RELEASE
         protected string APIUrl = "http://p1869.app.fit.ba/api";
@@ -76,15 +79,40 @@ namespace FIT_PONG.Mobile.APIServices
                 return default(T);
             }
         }
+        public async Task<T> GetAllPaged<T>(string _url)
+        {
+            if (String.IsNullOrEmpty(_url) || String.IsNullOrWhiteSpace(_url))
+                return default(T);
+            var url = _url;
+
+            try
+            {
+                return await url.WithBasicAuth(Username, Password).GetJsonAsync<T>();
+            }
+            catch (FlurlHttpException ex)
+            {
+                var errori = GetErrore(ex).Result;
+                await Application.Current.MainPage.DisplayAlert("Greška", errori, "OK");
+                return default(T);
+            }
+        }
         protected async Task<string> GetErrore(FlurlHttpException ex)
         {
+            //var staje = ex.GetResponseJsonAsync();
+            //var errori2 = await ex.GetResponseStringAsync();
+            //var errori3 = ex.Data;
+            
             var errori = await ex.GetResponseJsonAsync<Dictionary<string, string[]>>();
-            var _stringBilder = new StringBuilder();
-            foreach (var i in errori)
+            if (errori != null)
             {
-                _stringBilder.AppendLine($"{i.Key}, {string.Join(",", i.Value)}");
+                var _stringBilder = new StringBuilder();
+                foreach (var i in errori)
+                {
+                    _stringBilder.AppendLine($"{string.Join("\n", i.Value)}");
+                }
+                return _stringBilder.ToString();
             }
-            return _stringBilder.ToString();
+            return "Greška prilikom povezivanja";
         }
     }
 }
