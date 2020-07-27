51142c9752439addbea5f98d8886f333a044a6da
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-07-27 20:06:04 +0200

    Dodani odredjeni viewovi i viewmodeli za takmicenja

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaDetalji.xaml.cs
@@ -0,0 +1,23 @@
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FIT_PONG.Mobile.Views.Takmicenja
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TakmicenjaDetalji : ContentPage
+    {
+        TakmicenjaDetaljiViewModel viewModel;
+        public TakmicenjaDetalji(TakmicenjaDetaljiViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = viewModel = vm;
+        }
+    }
+}
---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaLista.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FIT_PONG.Mobile.Views.Takmicenja
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TakmicenjaLista : ContentPage
+    {
+        TakmicenjaListaViewModel viewModel;
+        public TakmicenjaLista()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new TakmicenjaListaViewModel();
+        }
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var gridlejaut = (BindableObject)sender;
+            var _takmicenje = (SharedModels.Takmicenja)gridlejaut.BindingContext;
+            await Navigation.PushAsync(new TakmicenjaDetalji(new TakmicenjaDetaljiViewModel(_takmicenje)));
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.ListaTakmicenja.Count == 0)
+                viewModel.IsBusy = true;
+            
+        }
+        
+    }
+}
