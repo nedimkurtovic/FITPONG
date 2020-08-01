55c235eeacc9845bee02b6fb8ee776f044e6af20
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-01 20:08:14 +0200

    Implementiran edit view za takmicenje

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Takmicenja/TakmicenjaEditViewModel.cs
@@ -0,0 +1,53 @@
+﻿using FIT_PONG.Mobile.APIServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FIT_PONG.Mobile.ViewModels.Takmicenja
+{
+    public class TakmicenjaEditViewModel:BaseViewModel
+    {
+        private SharedModels.Takmicenja Takmicenje { get; set; }
+        public TakmicenjaEditViewModel(SharedModels.Takmicenja _takmicenje)
+        {
+            RokPocetkaPrijave = _takmicenje.DatumPocetkaPrijava;
+            RokZavrsetkaPrijave = _takmicenje.DatumZavrsetkaPrijava;
+            DatumPocetka = _takmicenje.DatumPocetka;
+            MinimalniELO = _takmicenje.MinimalniELO;
+            Naziv = _takmicenje.Naziv;
+            Takmicenje = _takmicenje;
+        }
+        public TakmicenjeAPIService takmicenjeAPIService { get; set; } = new TakmicenjeAPIService();
+        private DateTime? _rokpocetkaprijave;
+        public DateTime? RokPocetkaPrijave { get => _rokpocetkaprijave; set => SetProperty(ref _rokpocetkaprijave, value); }
+
+        private DateTime? _rokzavrsetkaprijave;
+        public DateTime? RokZavrsetkaPrijave { get => _rokzavrsetkaprijave; set => SetProperty(ref _rokzavrsetkaprijave, value); }
+
+        private DateTime? _datumpocetka;
+        public DateTime? DatumPocetka { get => _datumpocetka; set => SetProperty(ref _datumpocetka, value); }
+
+        private int? _minimalniELO;
+        public int? MinimalniELO { get => _minimalniELO; set => SetProperty(ref _minimalniELO, value); }
+
+        private string _naziv;
+        public string Naziv { get => _naziv; set => SetProperty(ref _naziv, value); }
+
+        public bool DatumiPrijavaVisible() { return !Takmicenje.Inicirano ?? true; }
+        public async Task<SharedModels.Takmicenja> PozoviApi()
+        {
+            var obj = new SharedModels.Requests.Takmicenja.TakmicenjaUpdate
+            {
+                DatumPocetka = this.DatumPocetka,
+                MinimalniELO = this.MinimalniELO,
+                Naziv = this.Naziv,
+                RokPocetkaPrijave = this.RokPocetkaPrijave,
+                RokZavrsetkaPrijave = this.RokZavrsetkaPrijave
+            };
+            var rezultat = await takmicenjeAPIService.Update<SharedModels.Takmicenja>(Takmicenje.ID, obj);
+            return rezultat;
+        }
+    }
+}
---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaEdit.xaml.cs
@@ -0,0 +1,85 @@
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FIT_PONG.Mobile.Views.Takmicenja
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TakmicenjaEdit : ContentPage
+    {
+        TakmicenjaEditViewModel viewModel;
+        public TakmicenjaEdit(SharedModels.Takmicenja takm)
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new TakmicenjaEditViewModel(takm);
+            datumiPrijavaLayout.IsVisible = viewModel.DatumiPrijavaVisible();
+        }
+
+
+        private void cbDatumPP_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            dtpDatumPP.IsEnabled = !dtpDatumPP.IsEnabled;
+            if (!dtpDatumPP.IsEnabled)
+                viewModel.RokPocetkaPrijave = null;
+        }
+
+        private void cbDatumZP_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            dtpDatumZP.IsEnabled = !dtpDatumZP.IsEnabled;
+            if (!dtpDatumZP.IsEnabled)
+                viewModel.RokZavrsetkaPrijave = null;
+        }
+        private void cbDatumP_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            dtpDatumP.IsEnabled = !dtpDatumP.IsEnabled;
+            if (!dtpDatumZP.IsEnabled)
+                viewModel.DatumPocetka = null;
+        }
+
+        private async void btnSpasi_Clicked(object sender, EventArgs e)
+        {
+            var rezultat = await viewModel.PozoviApi();
+            if (rezultat != default(SharedModels.Takmicenja))
+            {
+                var mainStranica = Navigation.NavigationStack[1];
+                Navigation.InsertPageBefore(new TakmicenjaMain(new TakmicenjaDetaljiViewModel(rezultat)), mainStranica);
+                bool brisi = false;
+                //int brObrisanih = 0;
+                //for (int i = 0; i < Navigation.NavigationStack.Count+brObrisanih; i++)
+                //{
+                //    if (brisi)
+                //    {
+                //        Navigation.RemovePage(Navigation.NavigationStack[i]);
+                //        brObrisanih++;
+                //    }
+                //    if (Navigation.NavigationStack[i] is TakmicenjaMain && !brisi)
+                //        brisi = true;
+
+                //}
+                List<Page> listaBrisanja = new List<Page>();
+                foreach(var i in Navigation.NavigationStack)
+                {
+                    if (brisi)
+                        listaBrisanja.Add(i);
+                    if (i is TakmicenjaMain && !brisi)
+                        brisi = true;
+                }
+                //kad naleti na prvu main stavlja na true i sve ostale stranice brise poslije
+                var a = Navigation.NavigationStack[0].BindingContext as TakmicenjaListaViewModel;
+                if (a.DobaviTakmicenja.CanExecute(null))
+                    a.DobaviTakmicenja.Execute(null);
+                foreach (var i in listaBrisanja)
+                    Navigation.RemovePage(i);
+
+            }
+
+        }
+    }
+}
