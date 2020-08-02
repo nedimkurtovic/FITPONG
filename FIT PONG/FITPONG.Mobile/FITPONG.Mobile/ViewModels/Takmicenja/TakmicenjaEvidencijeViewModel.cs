4a8815ef672d0239d6b34f32ddb739a83a11bf3a
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-02 21:32:57 +0200

    Implementirana evidencija meca

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Takmicenja/TakmicenjaEvidencijeViewModel.cs
@@ -0,0 +1,32 @@
+﻿using FIT_PONG.Mobile.APIServices;
+using FIT_PONG.SharedModels.Requests.Takmicenja;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FIT_PONG.Mobile.ViewModels.Takmicenja
+{
+    public class TakmicenjaEvidencijeViewModel
+    {
+        public TakmicenjaEvidencijeViewModel(SharedModels.Takmicenja _takmicenje)
+        {
+            Takmicenje = _takmicenje;
+            ListaEvidencija = new ObservableCollection<EvidencijaMeca>();
+            GetEvidencije = new Command(async () => await DobaviEvidencije());
+        }
+        public TakmicenjeAPIService takmicenjeAPIService { get; set; } = new TakmicenjeAPIService();
+        public SharedModels.Takmicenja Takmicenje { get; set; }
+        public ObservableCollection<EvidencijaMeca> ListaEvidencija { get; set; }
+        public ICommand GetEvidencije { get; set; }
+        async Task DobaviEvidencije()
+        {
+            var rezultat = await takmicenjeAPIService.GetEvidencije(Takmicenje.ID);
+            foreach (var i in rezultat)
+                ListaEvidencija.Add(i);
+        }
+    }
+}
--- FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaEvidentirajMec.xaml.cs
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaEvidentirajMec.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja;
+using FIT_PONG.SharedModels.Requests.Takmicenja;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,39 @@ namespace FIT_PONG.Mobile.Views.Takmicenja
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TakmicenjaEvidentirajMec : ContentPage
     {
-        public TakmicenjaEvidentirajMec()
+        TakmicenjaEvidentirajMecViewModel viewModel;
+        public TakmicenjaEvidentirajMec(EvidencijaMeca _evidencija,SharedModels.Takmicenja takmicenje)
         {
             InitializeComponent();
+            BindingContext = viewModel = new TakmicenjaEvidentirajMecViewModel(_evidencija,takmicenje);
+
+        }
+
+        private async void Evidentiraj_Clicked(object sender, EventArgs e)
+        {
+            var rezultat = await viewModel.Evidencija();
+            if(rezultat)
+            {
+                //ovaj dio koda treba napravit extenzijom, vec sam ga 3 puta koristio
+                var mainStranica = Navigation.NavigationStack[1];
+                Navigation.InsertPageBefore(new TakmicenjaMain(new TakmicenjaDetaljiViewModel(viewModel.takmicenje)), mainStranica);
+                bool brisi = false;
+
+                List<Page> listaBrisanja = new List<Page>();
+                foreach (var i in Navigation.NavigationStack)
+                {
+                    if (brisi)
+                        listaBrisanja.Add(i);
+                    if (i is TakmicenjaMain && !brisi)
+                        brisi = true;
+                }
+                //kad naleti na prvu main stavlja na true i sve ostale stranice brise poslije
+                var a = Navigation.NavigationStack[0].BindingContext as TakmicenjaListaViewModel;
+                if (a.DobaviTakmicenja.CanExecute(null))
+                    a.DobaviTakmicenja.Execute(null);
+                foreach (var i in listaBrisanja)
+                    Navigation.RemovePage(i);
+            }
         }
     }
 }
