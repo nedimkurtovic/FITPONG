88c5c6ab3bd68106110387f59d1820ccc609ec42
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-02 21:30:49 +0200

    Implementiran prikaz tabele takmicenja

--- FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Takmicenja/TakmicenjaTabelaViewModel.cs
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Takmicenja/TakmicenjaTabelaViewModel.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using FIT_PONG.Mobile.APIServices;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace FIT_PONG.Mobile.ViewModels.Takmicenja
 {
-    public class TakmicenjaTabelaViewModel
+    public class TakmicenjaTabelaViewModel:BaseViewModel
     {
-        
+        public TakmicenjaTabelaViewModel(SharedModels.Takmicenja _takmicenje)
+        {
+            Takmicenje = _takmicenje;
+            TabelaStavke = new ObservableCollection<SharedModels.Requests.Takmicenja.TabelaStavka>();
+            DobaviTabelu = new Command(async () => await GetTabela());
+        }
+        public SharedModels.Takmicenja Takmicenje { get; set; }
+        public TakmicenjeAPIService takmicenjeAPIService { get; set; } = new TakmicenjeAPIService();
+
+        public ObservableCollection<SharedModels.Requests.Takmicenja.TabelaStavka> TabelaStavke { get; set; }
+        public ICommand DobaviTabelu { get; set; }
+        public async Task GetTabela()
+        {
+            var rezultat = await takmicenjeAPIService.GetTabela(Takmicenje.ID);
+            foreach (var i in rezultat)
+                TabelaStavke.Add(i);
+        }
+
     }
 }
--- FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaTabela.xaml.cs
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaTabela.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,13 @@ namespace FIT_PONG.Mobile.Views.Takmicenja
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TakmicenjaTabela : ContentPage
     {
-        public TakmicenjaTabela()
+        TakmicenjaTabelaViewModel viewModel;
+        public TakmicenjaTabela(SharedModels.Takmicenja _takmicenje)
         {
             InitializeComponent();
+            BindingContext = viewModel = new TakmicenjaTabelaViewModel(_takmicenje);
+            if (viewModel.DobaviTabelu.CanExecute(null))
+                viewModel.DobaviTabelu.Execute(null);
         }
     }
 }
