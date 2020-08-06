c20d10d2f0230cce48527b8a4a75bd81209b94b0
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-06 23:01:18 +0200

    Dodavanje novih objava

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Takmicenja/Objave/TakmicenjaObjaveDodajViewModel.cs
@@ -0,0 +1,60 @@
+﻿using FIT_PONG.Mobile.APIServices;
+using FIT_PONG.SharedModels;
+using FIT_PONG.SharedModels.Requests.Objave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FIT_PONG.Mobile.ViewModels.Takmicenja.Objave
+{
+    public class TakmicenjaObjaveDodajViewModel:BaseViewModel
+    {
+        public TakmicenjaObjaveDodajViewModel(SharedModels.Takmicenja _takmicenje)
+        {
+            Takmicenje = _takmicenje;
+            objaveAPIService = new BaseAPIService($"feeds/{Takmicenje.FeedID}/objave");
+        }
+        public BaseAPIService objaveAPIService { get; set; }
+        public SharedModels.Takmicenja Takmicenje { get; set; }
+
+        private string _naziv;
+        public string Naziv { get => _naziv; set => SetProperty(ref _naziv,value); }
+
+        private string _content;
+        public string Content { get => _content; set => SetProperty(ref _content,value); }
+
+        public async Task<SharedModels.Objave> DodajNovuObjavu()
+        {
+            if (!Validacija())
+                return default(SharedModels.Objave);
+
+            var request = new ObjaveInsertUpdate
+            {
+                Naziv = this.Naziv,
+                Content = this.Content
+            };
+
+            var rezultat = await objaveAPIService.Insert<SharedModels.Objave>(request);
+            return rezultat;
+        }
+        private bool Validacija()
+        {
+            var listaErrora = new List<string>();
+            if (String.IsNullOrEmpty(Naziv) || String.IsNullOrWhiteSpace(Naziv))
+                listaErrora.Add("Morate unijeti naziv objave");
+            if (String.IsNullOrEmpty(Content) || String.IsNullOrWhiteSpace(Content))
+                listaErrora.Add("Morate unijeti sadržaj objave");
+
+            if (listaErrora.Count == 0)
+                return true;
+            StringBuilder novi = new StringBuilder();
+            foreach (var i in listaErrora)
+                novi.AppendLine(i);
+            Application.Current.MainPage.DisplayAlert("Greška", novi.ToString(), "OK");
+            return false;
+        }
+    }
+}
---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/Objave/TakmicenjaObjaveDodaj.xaml.cs
@@ -0,0 +1,36 @@
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja.Objave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FIT_PONG.Mobile.Views.Takmicenja.Objave
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TakmicenjaObjaveDodaj : ContentPage
+    {
+        public TakmicenjaObjaveDodajViewModel viewModel;
+        public TakmicenjaObjaveDodaj(SharedModels.Takmicenja _takmicenje)
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new TakmicenjaObjaveDodajViewModel(_takmicenje);
+        }
+
+        private async void Dodaj_Clicked(object sender, EventArgs e)
+        {
+            var rezultat = await viewModel.DodajNovuObjavu();
+            if(rezultat != default(SharedModels.Objave))
+            {
+                var takmicenjamain = Navigation.NavigationStack[1] as TakmicenjaMain;
+                takmicenjamain.OsvjeziListuObjava();
+
+                Navigation.InsertPageBefore(new TakmicenjaObjaveDetalji(rezultat,viewModel.Takmicenje), this);
+                await Navigation.PopAsync();
+            }
+        }
+    }
+}
