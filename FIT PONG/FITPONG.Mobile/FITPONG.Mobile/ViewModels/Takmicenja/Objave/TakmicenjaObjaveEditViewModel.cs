1a4022f25844c71e4ba00af17164ccfc61ccd710
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-06 23:00:59 +0200

    Editovanje objava

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Takmicenja/Objave/TakmicenjaObjaveEditViewModel.cs
@@ -0,0 +1,61 @@
+﻿using FIT_PONG.Mobile.APIServices;
+using FIT_PONG.SharedModels.Requests.Objave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FIT_PONG.Mobile.ViewModels.Takmicenja.Objave
+{
+    public class TakmicenjaObjaveEditViewModel:BaseViewModel
+    {
+        public TakmicenjaObjaveEditViewModel(SharedModels.Objave _objava)
+        {
+            objaveAPIService = new BaseAPIService("objave");
+            Objava = _objava;
+            Naziv = Objava.Naziv;
+            Content = Objava.Content;
+        }
+        public BaseAPIService objaveAPIService { get; set; }
+        public SharedModels.Takmicenja Takmicenje { get; set; }
+
+        private string _naziv;
+        public string Naziv { get => _naziv; set => SetProperty(ref _naziv, value); }
+
+        private string _content;
+        public string Content { get => _content; set => SetProperty(ref _content, value); }
+        public SharedModels.Objave Objava { get; set; }
+
+        public async Task<SharedModels.Objave> UpdateObjavu()
+        {
+            if (!Validacija())
+                return default(SharedModels.Objave);
+
+            var request = new ObjaveInsertUpdate
+            {
+                Naziv = this.Naziv,
+                Content = this.Content
+            };
+
+            var rezultat = await objaveAPIService.Update<SharedModels.Objave>(Objava.ID, request,"PUT");
+            return rezultat;
+        }
+        private bool Validacija()
+        {
+            var listaErrora = new List<string>();
+            if (String.IsNullOrEmpty(Naziv) || String.IsNullOrWhiteSpace(Naziv))
+                listaErrora.Add("Morate unijeti naziv objave");
+            if (String.IsNullOrEmpty(Content) || String.IsNullOrWhiteSpace(Content))
+                listaErrora.Add("Morate unijeti sadržaj objave");
+
+            if (listaErrora.Count == 0)
+                return true;
+            StringBuilder novi = new StringBuilder();
+            foreach (var i in listaErrora)
+                novi.AppendLine(i);
+            Application.Current.MainPage.DisplayAlert("Greška", novi.ToString(), "OK");
+            return false;
+        }
+    }
+}
---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/Objave/TakmicenjaObjavaEdit.xaml.cs
@@ -0,0 +1,58 @@
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja.Objave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FIT_PONG.Mobile.Views.Takmicenja.Objave
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TakmicenjaObjavaEdit : ContentPage
+    {
+        TakmicenjaObjaveEditViewModel viewModel;
+        private readonly SharedModels.Takmicenja takmicenje;
+        public TakmicenjaObjavaEdit(SharedModels.Objave obj, SharedModels.Takmicenja _takmicenje)
+        {
+            InitializeComponent();
+            takmicenje = _takmicenje;
+            BindingContext = viewModel = new TakmicenjaObjaveEditViewModel(obj);
+        }
+
+        private async void Spasi_Clicked(object sender, EventArgs e)
+        {
+            var dajstek = Navigation.NavigationStack;
+            var ovojelista = Navigation.NavigationStack[2];
+
+            var rezultat = await viewModel.UpdateObjavu();
+            if (rezultat != default(SharedModels.Objave))
+            {
+                Page stranica=null;
+                foreach (var i in Navigation.NavigationStack)
+                {
+                    if (i is TakmicenjaObjaveDetalji)
+                        stranica = i;
+                }
+                Navigation.InsertPageBefore(new TakmicenjaObjaveDetalji(rezultat,takmicenje), stranica);
+                bool brisi = false;
+                List<Page> listaBrisanja = new List<Page>();
+                foreach (var i in Navigation.NavigationStack)
+                {
+                    if (brisi)
+                        listaBrisanja.Add(i);
+                    if (i is TakmicenjaObjaveDetalji && !brisi)
+                        brisi = true;
+                }
+                var takmicenjamain = Navigation.NavigationStack[1] as TakmicenjaMain;
+                takmicenjamain.OsvjeziListuObjava();
+
+                foreach (var i in listaBrisanja)
+                    Navigation.RemovePage(i);
+
+            }
+        }
+    }
+}
