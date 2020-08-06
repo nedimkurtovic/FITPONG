5859a8da39a4e2fb0962910a1285762a9a6d7b09
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-06 22:53:59 +0200

    Uvedena helper metoda za osvjezavanje liste objava i novi tab za objave

--- FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaMain.xaml.cs
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/TakmicenjaMain.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using FIT_PONG.Mobile.ViewModels.Takmicenja;
+using FIT_PONG.Mobile.ViewModels.Takmicenja.Objave;
+using FIT_PONG.Mobile.Views.Takmicenja.Objave;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,7 +37,30 @@ namespace FIT_PONG.Mobile.Views.Takmicenja
             TakmicenjaEvidentirajMecStranica.Title = "Evidentiraj meč";
             Children.Add(TakmicenjaEvidentirajMecStranica);
 
+            var TakmicenjaObjaveStranica = new TakmicenjaObjave(model.Takmicenje);
+            TakmicenjaObjaveStranica.Title = "Objave";
+            Children.Add(TakmicenjaObjaveStranica);
+
             Title = model.Takmicenje.Naziv;
+        }
+
+        public void OsvjeziListuObjava()
+        {
+            var children = Children;
+            foreach (var i in children)
+            {
+                if (i is TakmicenjaObjave)
+                {
+                    var b = i.BindingContext as TakmicenjaObjaveViewModel;
+                    if (b.DobaviObjaveKomanda.CanExecute(null))
+                    {
+                        b.DobaviObjaveKomanda.Execute(null);
+                        return;
+                    }
+                }
+      
+            }
+
         }
     }
 }
