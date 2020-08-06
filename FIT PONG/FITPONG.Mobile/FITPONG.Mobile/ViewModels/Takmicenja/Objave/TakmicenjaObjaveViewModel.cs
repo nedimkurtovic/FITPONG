e5fbb195ad52222ebfc3677879d6646c04cda675
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-06 22:59:17 +0200

    Prikaz liste objava

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Takmicenja/Objave/TakmicenjaObjaveViewModel.cs
@@ -0,0 +1,69 @@
+﻿using FIT_PONG.Mobile.APIServices;
+using FIT_PONG.SharedModels;
+using FIT_PONG.SharedModels.Requests;
+using FIT_PONG.SharedModels.Requests.Objave;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace FIT_PONG.Mobile.ViewModels.Takmicenja.Objave
+{
+    public class TakmicenjaObjaveViewModel:BaseViewModel
+    {
+        public ObservableCollection<SharedModels.Objave> ListaObjava { get; set; }
+        public TakmicenjaObjaveViewModel(SharedModels.Takmicenja _takmicenje)
+        {
+            ListaObjava = new ObservableCollection<SharedModels.Objave>();
+            Takmicenje = _takmicenje;
+            objaveAPIService = new BaseAPIService($"feeds/{_takmicenje.FeedID}/objave");
+            iducaStranica = "";
+            DobaviObjaveKomanda = new Command(async () => await dobaviObjaveFunkcija());
+            DobaviJosObjavaKomanda = new Command(async () => await dobaviJosObjavaFunkcija());
+            btnDobaviJosVisible = false;
+        }
+        public BaseAPIService objaveAPIService { get; set; }
+        public SharedModels.Takmicenja Takmicenje { get; set; }
+
+        private string _naziv;
+        public string Naziv { get=>_naziv; set => SetProperty(ref _naziv,value); }
+
+        private string iducaStranica;
+        public ICommand DobaviObjaveKomanda { get; set; }
+        public ICommand DobaviJosObjavaKomanda { get; set; }
+
+        public bool Vlasnik()
+        {
+            return BaseAPIService.ID == Takmicenje.KreatorID;
+        }
+        public bool btnDobaviJosVisible { get; set; }
+        async Task dobaviObjaveFunkcija()
+        {
+            ListaObjava.Clear();
+            var searchReq = new ObjaveSearch
+            {
+                Naziv = this.Naziv
+            };
+            var rezultat = await objaveAPIService.GetAll<PagedResponse<SharedModels.Objave>>(searchReq);
+            if(rezultat != default(PagedResponse<SharedModels.Objave>))
+                dodajUListu(rezultat);
+        }
+        async Task dobaviJosObjavaFunkcija()
+        {
+            var rezultat = await objaveAPIService.GetAllPaged<PagedResponse<SharedModels.Objave>>(iducaStranica);
+            if (rezultat != default(PagedResponse<SharedModels.Objave>))
+                dodajUListu(rezultat);
+        }
+        private void dodajUListu(PagedResponse<SharedModels.Objave> rez)
+        {
+            foreach (var i in rez.Stavke)
+                ListaObjava.Add(i);
+            iducaStranica = rez.IducaStranica?.ToString();
+            btnDobaviJosVisible = iducaStranica == null ? false : true;
+        }
+    }
+}
---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/Objave/TakmicenjaObjave.xaml.cs
@@ -0,0 +1,54 @@
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja;
+using FIT_PONG.Mobile.ViewModels.Takmicenja.Objave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FIT_PONG.Mobile.Views.Takmicenja.Objave
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TakmicenjaObjave : ContentPage
+    {
+        TakmicenjaObjaveViewModel viewModel;
+        public TakmicenjaObjave(SharedModels.Takmicenja _takmicenje)
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new TakmicenjaObjaveViewModel(_takmicenje);
+            btnDobaviJos.IsVisible = false;
+            btnDodaj.IsVisible = viewModel.Vlasnik();
+        }
+
+        private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            if (viewModel.DobaviObjaveKomanda.CanExecute(null))
+                viewModel.DobaviObjaveKomanda.Execute(null);
+            btnDobaviJos.IsVisible = viewModel.btnDobaviJosVisible;
+        }
+
+
+        private void btnDobaviJos_Clicked(object sender, EventArgs e)
+        {
+            if (viewModel.DobaviJosObjavaKomanda.CanExecute(null))
+                viewModel.DobaviJosObjavaKomanda.Execute(null);
+            btnDobaviJos.IsVisible = viewModel.btnDobaviJosVisible;
+        }
+
+        private async void btnDodaj_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TakmicenjaObjaveDodaj(viewModel.Takmicenje));
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var djesmo = Navigation.NavigationStack;
+            var steklejaut = (BindableObject)sender;
+            var objekatKliknuti = (SharedModels.Objave)steklejaut.BindingContext;
+            await Navigation.PushAsync(new TakmicenjaObjaveDetalji(objekatKliknuti, viewModel.Takmicenje));
+        }
+    }
+}
