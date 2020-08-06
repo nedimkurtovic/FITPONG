5a1a1b41b92baa07a4ab8caff99c3e1ff81de68c
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-06 23:00:01 +0200

    Prikaz detalja objave

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/ViewModels/Takmicenja/Objave/TakmicenjaObjaveDetaljiViewModel.cs
@@ -0,0 +1,32 @@
+﻿using FIT_PONG.Mobile.APIServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIT_PONG.Mobile.ViewModels.Takmicenja.Objave
+{
+    public class TakmicenjaObjaveDetaljiViewModel:BaseViewModel
+    {
+        public TakmicenjaObjaveDetaljiViewModel(SharedModels.Objave _objava , SharedModels.Takmicenja _takmicenje)
+        {
+            Objava = _objava;
+            Takmicenje = _takmicenje;
+            objaveAPIService = new BaseAPIService("objave");
+        }
+
+        public bool Vlasnik()
+        {
+            return BaseAPIService.ID == Takmicenje.KreatorID;
+        }
+        public BaseAPIService objaveAPIService { get; set; }
+        public SharedModels.Objave Objava { get; set; }
+        public SharedModels.Takmicenja Takmicenje { get; set; }
+
+        public async Task<SharedModels.Objave> ObrisiObjavu()
+        {
+            var rezultat = await objaveAPIService.Delete<SharedModels.Objave>(Objava.ID);
+            return rezultat;
+        }
+    }
+}
---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Takmicenja/Objave/TakmicenjaObjaveDetalji.xaml.cs
@@ -0,0 +1,48 @@
+﻿using FIT_PONG.Mobile.ViewModels.Takmicenja.Objave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FIT_PONG.Mobile.Views.Takmicenja.Objave
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TakmicenjaObjaveDetalji : ContentPage
+    {
+        private TakmicenjaObjaveDetaljiViewModel viewModel;
+        public TakmicenjaObjaveDetalji(SharedModels.Objave _objava,SharedModels.Takmicenja takmicenje)
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new TakmicenjaObjaveDetaljiViewModel(_objava,takmicenje);
+
+            dodatniLayout.IsVisible = viewModel.Vlasnik();
+
+        }
+
+        private async void btnEdit_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TakmicenjaObjavaEdit(viewModel.Objava,viewModel.Takmicenje));
+        }
+
+        private async void btnObrisi_Clicked(object sender, EventArgs e)
+        {
+
+            var rez = await Application.Current.MainPage.DisplayAlert("Upozorenje", "Ovo je nepovratna radnja, nastaviti?", "DA", "NE");
+            if (rez)
+            {
+                var rezultat = await viewModel.ObrisiObjavu();
+                if (rezultat != default(SharedModels.Objave))
+                {
+                    var takmicenjamain = Navigation.NavigationStack[1] as TakmicenjaMain;
+                    takmicenjamain.OsvjeziListuObjava();
+
+                    await Navigation.PopAsync();
+                }
+            }
+        }
+    }
+}
