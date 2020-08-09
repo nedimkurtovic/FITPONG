832ee52ad9a33d1b51b8810dd49f3f39692fe9ea
Nedim Kurtović <nedim.kurtovic@edu.fit.ba>
2020-08-09 23:15:25 +0200

    Implementirana lampica
    Postoji problem sa dobavljanjem inicijalne vrijednosti sa LampicaHuba, iz nekog razloga ako se u OnAppearing ili u konstruktoru Naslovnica pagea pozove funkcija GetTrenutnuSliku, ne okida se event koji docekuje trenutnu vrijednost. potrebno istraziti i rijesiti problem

---
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/APIServices/LampicaService.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIT_PONG.Mobile.APIServices
+{
+    public class LampicaService
+    {
+        public event EventHandler<MessageEventArgs> StigloTrenutnoStanje;
+        public event EventHandler<MessageEventArgs> PromijenjenaSlika;
+        public event EventHandler<MessageEventArgs> PromijenjenaPoruka;
+        public event EventHandler<MessageEventArgs> UgasenaKonekcija;
+
+
+        HubConnection hubKonekcija;
+        public string TrenutnaSlika { get; set; }
+        Random random;
+#if DEBUG
+        //protected string hubUrl = "http://localhost:5766/lampica";
+        protected string hubUrl = "http://localhost:4260/lampica";
+#endif
+#if RELEASE
+        protected string hubUrl = "http://p1869.app.fit.ba/lampica";
+#endif
+        public void Init()
+        {
+            random = new Random();
+            IsConnected = false;
+            TrenutnaSlika = "lampicaoff2.jpg";
+
+            hubKonekcija = new HubConnectionBuilder()
+                .WithUrl(hubUrl).Build();
+
+            hubKonekcija.Closed += async (error) =>
+            {
+                UgasenaKonekcija?.Invoke(this, new MessageEventArgs("Disconnected.."));
+                IsConnected = false;
+                await Task.Delay(random.Next(0, 5) * 1000);
+                try
+                {
+                    await ConnectAsync();
+                }
+                catch (Exception ex)
+                {
+                    // Exception!
+                }
+            };
+
+            hubKonekcija.On<string>("trenutnostanje", (finalnisrc) =>
+            {
+                var ispravljeniFajl = finalnisrc.Substring(finalnisrc.LastIndexOf("/") + 1);
+                StigloTrenutnoStanje?.Invoke(this, new MessageEventArgs(ispravljeniFajl));
+            });
+
+            hubKonekcija.On<string>("PromjenaPoruke", (poruka) =>
+            {
+                PromijenjenaPoruka?.Invoke(this, new MessageEventArgs(poruka));
+            });
+
+            hubKonekcija.On<string>("PromjenaStatusa", (finalnisrc) =>
+            {
+                //Obzirom da se salje /lampica/imefajla.png moram doc do samo ovog imefajla.png za ove 
+                //mobile varijane, nsiam siguran kako se ponasa kad stavis u folder sliku
+                var ispravljeniFajl = finalnisrc.Substring(finalnisrc.LastIndexOf("/") + 1);
+                PromijenjenaSlika?.Invoke(this, new MessageEventArgs(ispravljeniFajl));
+            });
+        }
+
+        public bool IsConnected { get; set; }
+
+        public async Task ConnectAsync()
+        {
+            if (IsConnected)
+                return;
+
+            await hubKonekcija.StartAsync();
+            IsConnected = true;
+
+        }
+
+        public async Task DisconnectAsync()
+        {
+            if (!IsConnected)
+                return;
+            await hubKonekcija.StopAsync();
+            IsConnected = false;
+
+        }
+
+        public async Task GetTrenutnuSlikuAsync()
+        {
+            if (!IsConnected)
+                return;
+            await hubKonekcija.InvokeAsync("VratiTrenutno");
+        }
+
+        public async Task PromijeniStanjeAsync(string Trenutnapic)
+        {
+            if (!IsConnected)
+                return;
+            var userName = BaseAPIService.Username;
+            await hubKonekcija.InvokeAsync("PromijeniStanje","/lampice/" + Trenutnapic,userName);
+        }
+    }
+}
--- FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Naslovnica.xaml.cs
+++ FIT PONG/FITPONG.Mobile/FITPONG.Mobile/Views/Naslovnica.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using FIT_PONG.Mobile.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,59 @@ namespace FIT_PONG.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Naslovnica : ContentPage
     {
+        NaslovnicaViewModel viewModel;
         public Naslovnica()
         {
             InitializeComponent();
+            BindingContext = viewModel = new NaslovnicaViewModel();
+            viewModel.lampicaServis.ConnectAsync();
+            
+
+            viewModel.lampicaServis.StigloTrenutnoStanje += LampicaServis_StigloTrenutnoStanje;
+            viewModel.lampicaServis.PromijenjenaSlika += LampicaServis_PromijenjenaSlika;
+            viewModel.lampicaServis.PromijenjenaPoruka += LampicaServis_PromijenjenaPoruka;
+
+            //Device.StartTimer(TimeSpan.FromSeconds(5), (Func<bool>)(() =>
+            //{
+            //    KaruselObavijesti.Position = (KaruselObavijesti.Position + 1) % viewModel.ListaPoruka.Count;
+            //    return true;
+            //}));
+        }
+        protected async override void OnAppearing()
+        {
+            await viewModel.lampicaServis.GetTrenutnuSlikuAsync();
+            base.OnAppearing();
+        }
+        private void LampicaServis_PromijenjenaPoruka(object sender, APIServices.MessageEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                lblPoruka.Text = e.Message;
+            });
+        }
+
+        private void LampicaServis_PromijenjenaSlika(object sender, APIServices.MessageEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var sours = e.Message;
+                if (DeviceInfo.Platform == DevicePlatform.UWP)
+                    sours = "Assets/" + e.Message;
+                SlikaLampica.Source = sours;
+                viewModel.lampicaServis.TrenutnaSlika = e.Message;
+            });
+        }
+
+        private void LampicaServis_StigloTrenutnoStanje(object sender, APIServices.MessageEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                var sours = e.Message;
+                if (DeviceInfo.Platform == DevicePlatform.UWP)
+                    sours = "Assets/" + e.Message;
+                SlikaLampica.Source = sours;
+                viewModel.lampicaServis.TrenutnaSlika = e.Message;
+            });
         }
     }
 }
